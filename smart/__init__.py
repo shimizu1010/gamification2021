281270366c5cdb401511d8ee604d0c0d534c3183
shimizu1010 <10shimizu10+github@gmail.com>
2022-02-01 11:59:30 +0900

    最終的な結果画面の実装

--- smart/__init__.py
+++ smart/__init__.py
@@ -12,12 +12,15 @@ class Constants(BaseConstants):
 class Subsession(BaseSubsession):
     pass
 
+
 class Group(BaseGroup):
     total_smart = models.IntegerField()
     total_meta_smart = models.IntegerField()
     total_fakenews = models.IntegerField()
     total_meta_fakenews = models.IntegerField()
-    #majority = models.IntegerField(initial=1)
+    number = models.IntegerField(initial=0)
+    smart_text = models.StringField()
+    fakenews_text = models.StringField()
 
     def compute(self):
         smarts = [p.smart for p in self.get_players()]
@@ -25,9 +28,6 @@ class Group(BaseGroup):
 
         self.total_smart = sum(smarts)
         print('total_smartの値は', self.total_smart)
-
-        #if 0.5 < self.total_smart % max([p.id_in_group for p in self.get_players()]):
-            #self.majority += 1
 
         meta_smarts = [p.meta_smart for p in self.get_players()]
         self.total_meta_smart = sum(meta_smarts)
@@ -38,12 +38,18 @@ class Group(BaseGroup):
         meta_fakenewses = [p.meta_fakenews for p in self.get_players()]
         self.total_meta_fakenews = sum(meta_fakenewses)
     
+    def result_compute(self):
+        if 0.5 <= (self.total_smart / self.number):
+            self.smart_text = '平均よりも賢いと考えている人が多数派です'
+        else :
+            self.smart_text = '平均よりも賢くないと考えている人が多数派です'
+        if 0.5 <= (self.total_fakenews / self.number):
+            self.fakenews_text = '平均よりもフェイクニュースに騙されると考えている人が多数派です'
+        else :
+            self.fakenews_text = '平均よりもフェイクニュースに騙されないと考えている人が多数派です'
 
-    #def check(self):
-     #   self.meta_smart = [p.meta_smart for p in self.get_players()]
-      #  if self.meta_smart == 1:
-       #     if 0.5 <= (self.total_smart / player.id_in_group):
-        #        answer_smart = 0
+
+
 
 
 
@@ -52,7 +58,6 @@ class Player(BasePlayer):
     smart = models.IntegerField(choices=[[0, '賢くない'],[1, '賢い']], label='あなたは平均的な人々と比べて賢いですか？', widget=widgets.RadioSelect, initial=0)
 
     meta_smart = models.IntegerField(choices=[[0, '少数派'],[1, '多数派']], label='前の質問で、あなたが答えた回答は多数派か少数派のどちらだと思いますか？', widget=widgets.RadioSelect, initial=0)
-    
 
     fakenews = models.IntegerField(choices=[[0, '思う'],[1, '思わない']], label='あなたは平均的な人々と比べてフェイクニュースに騙されるほうだと思いますか？', widget=widgets.RadioSelect, initial=0)
 
@@ -77,6 +82,8 @@ class Survery1(Page):
     @staticmethod
     def before_next_page(self, timeout_happened):
         self.group.compute()
+        self.group.number += 1
+        print('＋1')
 
 class Survery2(Page):
     form_model = 'player'
@@ -98,6 +105,7 @@ class Survery4(Page):
     @staticmethod
     def before_next_page(self, timeout_happened):
         self.group.compute()
+        self.group.result_compute()
 
 
 
@@ -119,7 +127,10 @@ class Higher_Threshold_Lastpage(Page):
         print("idは",player.id_in_group)
         return player.id_in_group >= 3
 
+
 class Lower_Threshold_Lastpage(Page):
     pass
+
+
 
 page_sequence = [Start_Page, Survery1, Survery2, Result1, Survery3, Survery4, Result2, Higher_Threshold_Lastpage, Lower_Threshold_Lastpage]
